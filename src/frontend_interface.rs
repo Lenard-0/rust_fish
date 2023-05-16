7224e4febbec54d429e11703efabbdc59b078b51
Lenard-0 <lenard@insanemarketing.com.au>
2023-05-17 08:02:00 +0930

    testing where it breaks

--- src/frontend_interface.rs
+++ src/frontend_interface.rs
@@ -19,5 +19,6 @@ pub fn parse_board(board: Vec<Vec<String>>) -> Result<Vec<Vec<Option<Piece>>>, S
         }
         parsed_board.push(new_row);
     }
-    return Ok(parsed_board)
+    return Err("parsing board worked".to_string())
+    // return Ok(parsed_board)
 }
