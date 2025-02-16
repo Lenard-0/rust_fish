b554d363e88bdd0c216c35c4523d831fad7d9cbf
Lenard0 <lachie.hislop@gmail.com>
2025-02-17 10:29:08 +1030

    passed all check cases

--- src/chess_functionality/moves/king.rs
+++ src/chess_functionality/moves/king.rs
@@ -1,5 +1,5 @@
 use crate::{Piece, PieceType};
-use super::{check::king_is_checked, search_for_moves::search_for_moves, Move};
+use super::{check::king_is_checked, move_piece, search_for_moves::search_for_moves, unmove_piece, Move};
 
 #[derive(Clone)]
 pub struct CastleState {
@@ -33,34 +33,37 @@ pub fn calculate_king_moves(
     castle_state: &mut CastleState,
 ) -> Result<Vec<Move>, String> {
     let possible_moves = search_for_moves(board, ir, ic, whites_turn, PieceType::King.directions(), 1)?;
-    if king_is_checked(board, whites_turn, previous_move, castle_state)? {
-        return Ok(possible_moves)
-    }
-    let possible_castling_moves = calculate_castling_moves(board, whites_turn, castle_state)?;
+    let possible_castling_moves = calculate_castling_moves(board, whites_turn, previous_move, castle_state)?;
     Ok(vec![possible_moves, possible_castling_moves].concat())
 }
 
 fn calculate_castling_moves(
     board: &mut Vec<Vec<Option<Piece>>>,
     whites_turn: bool,
-    castle_state: &CastleState,
+    previous_move: &Option<Move>,
+    castle_state: &mut CastleState,
 ) -> Result<Vec<Move>, String> {
     let mut possible_moves = Vec::new();
     if whites_turn {
         if !castle_state.white_king_moved {
             if !castle_state.white_right_rook_moved && board[7][5].is_none() && board[7][6].is_none() {
-                // check if each of those squares are not under attack
-
-                // make sure rook is in fact there as it could have been taken
                 match board[7][7] {
-                    Some(Piece::White(PieceType::Rook)) => possible_moves.push(Move { current_pos: (7, 4), new_pos: (7, 6) }),
+                    Some(Piece::White(PieceType::Rook)) => {
+                        if all_tiles_free_from_check(vec![(7, 5), (7, 6)], (7, 4), board, whites_turn, previous_move, castle_state)? {
+                            possible_moves.push(Move { current_pos: (7, 4), new_pos: (7, 6) })
+                        }
+                    },
                     _ => {}
                 };
             }
             if !castle_state.white_left_rook_moved && board[7][1].is_none() && board[7][2].is_none() && board[7][3].is_none() {
                 // make sure rook is in fact there as it could have been taken
                 match board[7][0] {
-                    Some(Piece::White(PieceType::Rook)) => possible_moves.push(Move { current_pos: (7, 4), new_pos: (7, 2) }),
+                    Some(Piece::White(PieceType::Rook)) => {
+                        if all_tiles_free_from_check(vec![(7, 1), (7, 2), (7, 3)], (7, 4), board, whites_turn, previous_move, castle_state)? {
+                            possible_moves.push(Move { current_pos: (7, 4), new_pos: (7, 2) })
+                        }
+                    },
                     _ => {}
                 };
             }
@@ -69,13 +72,21 @@ fn calculate_castling_moves(
         if !castle_state.black_king_moved {
             if !castle_state.black_right_rook_moved && board[0][5].is_none() && board[0][6].is_none() {
                 match board[0][7] {
-                    Some(Piece::Black(PieceType::Rook)) => possible_moves.push(Move { current_pos: (0, 4), new_pos: (0, 6) }),
+                    Some(Piece::Black(PieceType::Rook)) => {
+                        if all_tiles_free_from_check(vec![(0, 5), (0, 6)], (0, 4), board, whites_turn, previous_move, castle_state)? {
+                            possible_moves.push(Move { current_pos: (0, 4), new_pos: (0, 6) })
+                        }
+                    },
                     _ => {}
                 };
             }
             if !castle_state.black_left_rook_moved && board[0][1].is_none() && board[0][2].is_none() && board[0][3].is_none() {
                 match board[0][0] {
-                    Some(Piece::Black(PieceType::Rook)) => possible_moves.push(Move { current_pos: (0, 4), new_pos: (0, 2) }),
+                    Some(Piece::Black(PieceType::Rook)) => {
+                        if all_tiles_free_from_check(vec![(0, 1), (0, 2), (0, 3)], (0, 4), board, whites_turn, previous_move, castle_state)? {
+                            possible_moves.push(Move { current_pos: (0, 4), new_pos: (0, 2) })
+                        }
+                    },
                     _ => {}
                 };
             }
@@ -83,4 +94,28 @@ fn calculate_castling_moves(
     }
 
     Ok(possible_moves)
+}
+
+fn all_tiles_free_from_check(
+    tiles: Vec<(usize, usize)>,
+    king_position: (usize, usize),
+    board: &mut Vec<Vec<Option<Piece>>>,
+    whites_turn: bool,
+    previous_move: &Option<Move>,
+    castle_state: &mut CastleState
+) -> Result<bool, String> {
+    if king_is_checked(board, whites_turn, previous_move, castle_state) == Ok(true) {
+        return Ok(false)
+    }
+
+    for tile in tiles {
+        let taken_piece = move_piece(&Move { current_pos: king_position, new_pos: tile }, board);
+        if king_is_checked(board, whites_turn, previous_move, castle_state)? {
+            unmove_piece(&Move { current_pos: king_position, new_pos: tile }, board, taken_piece);
+            return Ok(false)
+        }
+        unmove_piece(&Move { current_pos: king_position, new_pos: tile }, board, taken_piece);
+    }
+
+    Ok(true)
 }
